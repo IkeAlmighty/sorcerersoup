f4fb6724d1afb81832ff51101e5de0ce3285000c
Izik <IsaacYates7@gmail.com>
2022-04-18 19:23:16 -0500

    added react-toastify globally, and added copy-to-clipboard

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import "../public/global.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Component {...pageProps} />
+      <ToastContainer />
+    </div>
+  );
 }
 
 export default MyApp;
