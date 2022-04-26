33c71ccb468598b1d1c5241ce23e66bdaafddb7e
Izik <IsaacYates7@gmail.com>
2022-04-25 21:39:12 -0500

    made home page real

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,44 @@
 import Link from "next/link";
-import { useEffect } from "react";
-import { logToLocalHistory, useLocalHistory } from "../lib/react-localhistory";
+import { useEffect, useState } from "react";
+import { useLocalHistory } from "../lib/hooks/react-localhistory";
 
 export default function Home() {
   const localHistory = useLocalHistory();
+  const [characters, setCharacters] = useState([]);
+
+  async function getCharacterNameFromLink(link) {
+    const parsed = link.split("?");
+    const query = parsed[1];
+
+    const apiResponse = await fetch(`/api/character?${query}`);
+
+    if (apiResponse.status !== 200) return null;
+
+    const character = await apiResponse.json();
+    return character.name;
+  }
+
+  useEffect(() => {
+    if (!localHistory) return;
+    const links = localHistory.filter(
+      (val, index) => localHistory.indexOf(val) === index
+    ); //filters repeated elements out
+
+    async function fetchAndSetCharacters() {
+      const _characters = [];
+
+      for (let i = 0; i < links.length; i++) {
+        let character = await getCharacterNameFromLink(links[i]);
+
+        if (character) _characters.push(character);
+      }
+
+      setCharacters(_characters);
+    }
+
+    fetchAndSetCharacters();
+  }, [localHistory]);
+
   return (
     <div className="h-screen">
       <div className="text-center my-6 header">
@@ -26,15 +61,18 @@ export default function Home() {
 
           <div className="my-[200px]">
             <h2>Recently Visited on this Device:</h2>
-            {localHistory
+            {characters.map((c) => {
+              return <div key={c.query} className=""></div>;
+            })}
+            {/* {localHistory
               ?.filter((val, index) => localHistory.indexOf(val) === index) //filter repeated elements out
               .map((link) => (
-                <div className="text-sm my-2">
+                <div key={link} className="text-sm my-2">
                   <Link href={link}>
-                    <a>{link}</a>
+                    <a>{getCharacterNameFromLink(link)}</a>
                   </Link>
                 </div>
-              ))}
+              ))} */}
           </div>
         </div>
       </div>
