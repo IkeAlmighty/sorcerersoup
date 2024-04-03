a8b9250cd44df66ac6a25997221d6f03c01d8171
ikealmighty <isaacyates7@gmail.com>
2024-04-03 13:00:02 -0500

    First Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,145 @@
+<script>
+	export let data;
+
+	let filteredData = [];
+	let tiersSelected = { 3: true, 2: false, 1: false, S: false };
+	let searchBarValue;
+
+	filterSpells();
+
+	function toggleTiersSelectedAndFilter(tierToggles) {
+		// toggle checkbox if this function was triggered by clicking one:
+		tierToggles.forEach((tier) => (tiersSelected[tier] = !tiersSelected[tier]));
+		filterSpells();
+	}
+
+	function filterSpellsByTier() {
+		let filtered = data.data.filter((spell) => {
+			function determineSpellTier(s) {
+				let manaCost = s['mana cost'];
+				if (!manaCost) return 'S';
+				else if (manaCost < 50) return 3;
+				else if (manaCost < 75) return 2;
+				else if (manaCost < 150) return 1;
+				else return 'S';
+			}
+
+			let tier = determineSpellTier(spell);
+			return tiersSelected[tier];
+		});
+
+		return filtered;
+	}
+
+	function filterSpellsBySearchbar() {
+		// if the searchbar is empty, then reset the list and return
+		if (!searchBarValue || searchBarValue.length === 0) return data.data;
+
+		//start out with all spells
+		let filtered = data.data;
+		let words = searchBarValue.toLowerCase();
+
+		//filter out spells that don't have a word match
+		filtered = data.data.filter((spell) => {
+			let keywords = JSON.stringify(spell).toLowerCase();
+			return keywords.includes(words);
+		});
+
+		return filtered;
+	}
+
+	function filterSpells() {
+		let filteredBySearchBar = filterSpellsBySearchbar();
+		let filteredByTier = filterSpellsByTier();
+
+		filteredData = [];
+
+		filteredBySearchBar.forEach((searchBarSpell) => {
+			filteredByTier.forEach((tierSpell) => {
+				if (searchBarSpell === tierSpell) filteredData.push(searchBarSpell);
+			});
+		});
+	}
+</script>
+
+<div class="center">
+	<input
+		type="text"
+		placeholder="Search Here"
+		bind:value={searchBarValue}
+		on:input={() => filterSpells()}
+	/>
+	<br />
+
+	<label>
+		<input
+			type="checkbox"
+			name="tier"
+			checked={tiersSelected[3]}
+			on:click={(e) => toggleTiersSelectedAndFilter([3])}
+		/>
+		Tier 3
+	</label>
+	<label>
+		<input
+			type="checkbox"
+			name="tier"
+			checked={tiersSelected[2]}
+			on:click={(e) => toggleTiersSelectedAndFilter([2])}
+		/>
+		Tier 2
+	</label>
+	<label>
+		<input
+			type="checkbox"
+			name="tier"
+			checked={tiersSelected[1]}
+			on:click={(e) => toggleTiersSelectedAndFilter([1])}
+		/>
+		Tier 1
+	</label>
+	<label>
+		<input
+			type="checkbox"
+			name="tier"
+			checked={tiersSelected['S']}
+			on:click={(e) => toggleTiersSelectedAndFilter(['S'])}
+		/>
+		Tier S
+	</label>
+</div>
+
+{#each filteredData as spell}
+	<div class="spell-container">
+		<div>Spell Type: {spell['type']}</div>
+		<div>Mana Cost: {spell['mana cost'] || '--'}</div>
+		<div><span>Effect: </span>{spell.effect}</div>
+	</div>
+{/each}
+
+<style>
+	input[type='text'] {
+		display: block;
+		padding: 0.5rem;
+		width: 90%;
+		border: 1px solid black;
+		border-radius: 10px;
+		text-align: center;
+		margin: auto auto;
+	}
+
+	.center {
+		text-align: center;
+		max-width: 500px;
+		margin: auto auto;
+	}
+
+	.spell-container {
+		max-width: 500px;
+		margin: 2rem auto;
+	}
+
+	.name {
+		font-weight: bold;
+	}
+</style>
